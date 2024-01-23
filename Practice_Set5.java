4dad588209c4872ce371eaca809de556497fe6f5
anushal <125723541+anushal78@users.noreply.github.com>
2024-01-23 22:56:42 +0530

    Update Practice_Set5.java

--- Practice_Set5.java
+++ Practice_Set5.java
@@ -15,7 +15,7 @@ public class Practice_Set5 {
         int n = 4;
         for(int i=n;i>0;i--){
           for(int j=0;j<i;j++){
-              System.out.println("*");
+              System.out.print("*");
           }
           System.out.println("\n");
       }
